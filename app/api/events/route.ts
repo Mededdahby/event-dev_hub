6c574bf2a2715a4d91286b31222f7d3797e32b03
Mededdahby <mededdahby1@gmail.com>
2026-01-21 20:25:31 +0100

    add image upload cloudinary

---
+++ app/api/events/route.ts
@@ -0,0 +1,56 @@
+import { connectToDatabase } from "@/lib/mongodb";
+import { v2 as cloudinary } from "cloudinary";
+import { NextRequest, NextResponse } from "next/server";
+import { Event } from "@/database/event.model";
+
+export async function POST(req: NextRequest) {
+  try {
+    await connectToDatabase();
+
+    const event = await req.json();
+    //TODO: get the image from the form data
+    const file = event.get("image") as File;
+    //
+    if (!file) {
+      return NextResponse.json({ message: "Image file is required" });
+    }
+
+    //TODO : Transform the image to Buffer and upload it to Cloudinary
+
+    const arrayBuffer = await file.arrayBuffer();
+    const buffer = Buffer.from(arrayBuffer);
+
+    const uploadResults = await new Promise((resolve, reject) => {
+      cloudinary.uploader
+        .upload_stream(
+          { resource_type: "image", folder: "events" },
+          (error, result) => {
+            if (error) {
+              reject(error);
+            } else {
+              resolve(result);
+            }
+          },
+        )
+        .end(buffer);
+    });
+
+    event.image = (uploadResults as { secure_url: string }).secure_url;
+
+    const createdEvent = await Event.create(event);
+
+    return NextResponse.json(
+      { message: "Event created successfully", event: createdEvent },
+      { status: 201 },
+    );
+  } catch (e) {
+    console.error(e);
+    return NextResponse.json(
+      {
+        message: "Event creation failed!",
+        error: e instanceof Error ? e : "Unknown error",
+      },
+      { status: 500 },
+    );
+  }
+}
