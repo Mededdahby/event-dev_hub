7d01352797e6e0a6b41e69cdfdd4894d7e809e95
Mededdahby <mededdahby1@gmail.com>
2026-01-22 13:00:39 +0100

    impage upload couldinary

--- app/api/events/route.ts
+++ app/api/events/route.ts
@@ -1,12 +1,54 @@
-import { connectToDatabase } from "@/lib/mongodb";
 import { NextRequest, NextResponse } from "next/server";
+import { v2 as cloudinary } from "cloudinary";
+
+import { connectToDatabase } from "@/lib/mongodb";
 import { Event } from "@/database/event.model";
 
 export async function POST(req: NextRequest) {
   try {
     await connectToDatabase();
 
-    const event = await req.json(); // ✅ JSON body
+    const formData = await req.formData();
+
+    let event;
+
+    try {
+      event = Object.fromEntries(formData.entries());
+    } catch (e) {
+      return NextResponse.json(
+        { message: "Invalid JSON data format" },
+        { status: 400 },
+      );
+    }
+
+    const file = formData.get("image") as File;
+
+    if (!file)
+      return NextResponse.json(
+        { message: "Image file is required" },
+        { status: 400 },
+      );
+
+    // let tags = JSON.parse(formData.get("tags") as string);
+    // let agenda = JSON.parse(formData.get("agenda") as string);
+
+    const arrayBuffer = await file.arrayBuffer();
+    const buffer = Buffer.from(arrayBuffer);
+
+    const uploadResult = await new Promise((resolve, reject) => {
+      cloudinary.uploader
+        .upload_stream(
+          { resource_type: "image", folder: "DevEvent" },
+          (error, results) => {
+            if (error) return reject(error);
+
+            resolve(results);
+          },
+        )
+        .end(buffer);
+    });
+
+    event.image = (uploadResult as { secure_url: string }).secure_url;
 
     const createdEvent = await Event.create(event);
 
@@ -18,8 +60,8 @@ export async function POST(req: NextRequest) {
     console.error(e);
     return NextResponse.json(
       {
-        message: "Event creation failed!",
-        error: e instanceof Error ? e : "Unknown error",
+        message: "Event Creation Failed",
+        error: e instanceof Error ? e.message : "Unknown",
       },
       { status: 500 },
     );
